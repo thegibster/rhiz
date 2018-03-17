ae4492c7a4768fca8fdf28b184183c27f5bc6fe8
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-03-17 14:47:11 -0700

    moved server.js file into main app folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cookieSession = require('cookie-session');
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
+const keys = require('./server/config/keys');
 const PORT = process.env.PORT || 4000;
 const app = express();
 
@@ -20,10 +20,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect(keys.mongodbURI);
-require('./models/User');
-require('./services/googlePassport');
+require('./server/models/User');
+require('./server/services/googlePassport');
 
-require('./routes')(app);
+require('./server/routes')(app);
+
+// run this express heroku production
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static("client/build"));
+
+  // express will serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, function() {
     console.log('app listening on Port ' + PORT);

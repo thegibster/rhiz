1b315a2af3bbef827a5521d901b736287b6dee58
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-03-19 20:33:28 -0700

    removed server config file, added test header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1>Rhiz App Working</h1>
         <a href="/auth/google">Log in with Google</a>
+        <a href="/api/logout">Logout</a>
       </div>
     );
   }

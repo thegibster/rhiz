f43ad24ad1728c570a97665803fb65e67943774d
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-03-21 17:47:52 -0700

    added coverImage component, still need to style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import CoverImage from './common/CoverImage';
+import img12 from "../assets/images/img12.jpg";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <h1>Rhiz App Working</h1>
+        <CoverImage src={img12} alt="Tom" />
+        <h1>Rhiz</h1>
         <a href="/auth/google">Log in with Google</a>
         <a href="/api/logout">Logout</a>
       </div>

37444f704041ae98f7db174bce51d9495a70aff0
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-03-21 19:26:25 -0700

    resolved typos

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers'
 import "semantic-ui-css/semantic.min.css";
 import './components/common/common.css';
 

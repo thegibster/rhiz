4eea7d44132edc8590cbe8e1a8ed5ab17dfbf10f
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-03-22 15:23:10 -0700

    added styling to footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from './Header';
-import CoverImage from './common/CoverImage';
 import ParallaxImage from './common/ParallaxImage';
 import Footer from './common/Footer';
 import img12 from "../assets/images/img12.jpg";
@@ -12,7 +11,6 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Header />
-          {/* <CoverImage src={img12} alt="Tom" /> */}
           <ParallaxImage bgImage={img12} title="Rhiz" alt="parallaximg" />
           <h1>Rhiz</h1>
           <a href="/auth/google">Log in with Google</a>
--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -5,74 +5,76 @@ import {
   Container,
   Grid,
   Header,
-  List,
-  Visibility,
-  Transition
+  List
 } from "semantic-ui-react";
-// import "./footer.css";
 
 class Footer extends Component {
-
-  state = {
-    animation: null,
-    visible: false,
-    pass: "slide up",
-    reverse: "slide down"
-  };
-
   render() {
-    const { animation, visible, pass, reverse } = this.state;
-
-    return (
-      <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
-          <Segment inverted vertical style={{ padding: "2em 0em" }}>
-            <Container>
-              <Grid divided inverted>
-                <Grid.Row>
-                  <Grid.Column width={3}>
-                    <Header inverted as="h4" content="Company" />
-                    <List link>
-                      <List.Item>
-                        <Link to="/meet-the-team">Meet the Team</Link>
-                      </List.Item>
-                    </List>
-                  </Grid.Column>
-                  <Grid.Column width={3}>
-                    <Header as="h4" content="Events" />
-                    <List link>
-                      <List.Item>
-                        <Link to="/event-page/ATX">Austin</Link>
-                      </List.Item>
-                      <List.Item>
-                        <Link to="/event-page/LA">Los Angeles</Link>
-                      </List.Item>
-                      <List.Item>
-                        <Link to="/event-page/NY">New York</Link>
-                      </List.Item>
-                      <List.Item>
-                        <Link to="/event-page/SF">San Francisco</Link>
-                      </List.Item>
-                      <List.Item>
-                        <Link to="/event-page/SEA">Seattle</Link>
-                      </List.Item>
-                    </List>
-                  </Grid.Column>
-                  <Grid.Column width={7}>
-                    <Header as="h4">Contact Us</Header>
-                    <a
-                      className="email"
-                      href="mailto:lowkeendonovan@gmail.com?subject=Feedback"
-                    >
-                      Send us a message
+    return <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
+        <Segment inverted vertical style={{ padding: "2em 0em" }}>
+          <Container>
+            <Grid divided inverted>
+              <Grid.Row>
+                <Grid.Column width={5}>
+                  <Header inverted as="h1" content="Rhiz" />
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as="h4" content="Company" />
+                  <List link>
+                    <List.Item>
+                      <Link to="/about">About</Link>
+                    </List.Item>
+                    <List.Item>
+                      <Link to="/careers">Careers</Link>
+                    </List.Item>
+                    <List.Item>
+                      <Link to="/terms-of-service">Terms of Service</Link>
+                    </List.Item>
+                    <List.Item>
+                      <Link to="/privacy-policy">Privacy Policy</Link>
+                    </List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as="h4" content="Contact" />
+                  <List link>
+                    <List.Item>
+                      <Link to="/help">Help</Link>
+                    </List.Item>
+                    <List.Item>
+                      <Link to="/faq">FAQ</Link>
+                    </List.Item>
+                    <List.Item>
+                      <Link to="/contact">Contact</Link>
+                    </List.Item>
+                    <List.Item>
+                      <Link to="/partnerships">Partnerships</Link>
+                    </List.Item>
+                    <a className="email" href="mailto:lowkeendonovan@gmail.com?subject=Feedback">
+                      Email
                     </a>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row className="centered">© Rhiz</Grid.Row>
-              </Grid>
-            </Container>
-          </Segment>
-      </Container>
-    );
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as="h4">Business Tools</Header>
+                  <List link>
+                    <List.Item>
+                      <Link to="/pricing">Pricing</Link>
+                    </List.Item>
+                    <List.Item>
+                      <Link to="/starting-out">Starting Out</Link>
+                    </List.Item>
+                    <List.Item>
+                      <Link to="/self-awareness">Self-Awareness</Link>
+                    </List.Item>
+                  </List>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row className="centered">© Rhiz</Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
+      </Container>;
   }
 }
 

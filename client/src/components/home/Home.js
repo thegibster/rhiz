05d93ba635171538a8387b162a7a45917acd5444
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-04-06 18:52:48 -0700

    set up pricing cards, must do styling

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Container } from 'semantic-ui-react';
 import ParallaxImage from "../common/ParallaxImage";
-import img12 from "../../assets/images/img12.jpg";
+import landscape1 from "../../assets/images/landscape1.jpeg";
 import FeaturedCarousel from './FeaturedCarousel';
 import FeaturedCarousel2 from "./FeaturedCarousel2";
 import './home.css';
@@ -9,7 +9,7 @@ import './home.css';
 class Home extends Component {
   render() {
     return <div>
-        <ParallaxImage bgImage={img12} height={{ height: "250px" }} title="Rhiz Marketplace" alt="parallaximg" />
+        <ParallaxImage bgImage={landscape1} height={{ height: "250px" }} title="Rhiz Marketplace" alt="parallaximg" />
         <div style={{ height: "80px" }} />
         <Container>
           <h1 className="title">Featured Landscaping Listings</h1>
--- client/src/components/pricing/PriceCards.js
+++ client/src/components/pricing/PriceCards.js
@@ -1,29 +1,26 @@
 import React from "react";
-import Slider from "react-slick";
-import { Container, Card, Icon, Image } from "semantic-ui-react";
-import VerticalCard from "../common/VerticalCard";
+import { Container, Card, Icon, Divider, Button } from "semantic-ui-react";
 
-class PriceCard extends React.Component {
-  render() {
-    return <Container>
-        <Card>
-          <Image src="../../assets/images/interior1.jpg" />
-          <Card.Content>
-            <Card.Header>Daniel</Card.Header>
-            <Card.Meta>Joined in 2016</Card.Meta>
-            <Card.Description>
-              Daniel is a comedian living in Nashville.
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name="user" />
-              10 Friends
-            </a>
-          </Card.Content>
-        </Card>
-      </Container>;
-  }
+const PriceCard = props => {
+  return <Container>
+      <Card className="price-card-container">
+        <Card.Content style={{ height: "100px" }}>
+          <h2>{props.title}</h2>
+        </Card.Content>
+        <Card.Content>
+          <Card.Header>{props.header}</Card.Header>
+          <Card.Description>
+            {props.description}
+          </Card.Description>
+          <Button 
+            fluid basic color="green"
+            href={props.link}
+          >
+            Subscribe
+          </Button>
+        </Card.Content>
+      </Card>
+    </Container>;
 }
 
 export default PriceCard;

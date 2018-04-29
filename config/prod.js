70af83c3f47100c68d9d57c9ac7adf2cfccf03ee
Jacob Carter <jc@jacobcarter.com>
2018-04-29 14:44:59 -0700

    move restart to package.json

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,9 @@ module.exports = {
   googleClientID: process.env.GOOGLECLIENTID,
   googleClientSecret: process.env.GOOGLESECRET,
   mongodbURI: process.env.MONGODB_URI,
-  cookieKey: process.env.COOKIEKEY
+  cookieKey: process.env.COOKIEKEY,
+  linkedInClientID: process.env.LINKED_IN_CLIENT_ID,
+  linkedInClientSecret: process.env.LINKED_IN_CLIENT_SECRET,
+  facebookAppID: process.env.FACEBOOK_APP_ID,
+  facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
 }

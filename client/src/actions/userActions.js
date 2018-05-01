2d980376a357100178be3e07271d74f7febaf49d
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-04-30 17:54:32 -0700

    wiring up login action and reducer for local passport

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER, CREATE_LOGIN } from "./types";
+import { FETCH_USER, CREATE_LOGIN, LOGIN_USER } from "./types";
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/auth/current_user");
@@ -9,4 +9,11 @@ export const fetchUser = () => async dispatch => {
 export const createLogin = (loginInfo) => async dispatch => {
   const res = await axios.post("/auth/create", loginInfo);
   dispatch({ type: CREATE_LOGIN, payload: res.data });
+}
+
+export const loginUser = (loginInfo) => async dispatch => {
+  const res = await axios.post("/auth/login", loginInfo);
+  console.log("loginUser", loginInfo);
+  console.log("res", res);
+  dispatch({ type: LOGIN_USER, payload: res.data });
 }

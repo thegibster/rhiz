110b63d8445edc1d817f036be943c1a60cad31a8
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-06-03 14:42:38 -0700

    adjusted google api key

--- client/src/components/profile/MapContainer.js
+++ client/src/components/profile/MapContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { Embed } from 'semantic-ui-react';
+import keys from '../../config-client/keys';
 
 class MapContainer extends Component {
   render() {
-    let googleUrl = "https://www.google.com/maps/embed/v1/place?key=" + process.env.GOOGLE_MAPS_API_KEY + "&q=Eiffel+Tower,Paris+France";
+    let googleUrl = "https://www.google.com/maps/embed/v1/place?key=" + keys.googleAPIKey + "&q=Eiffel+Tower,Paris+France";
     return <div>
-        <Embed active url={googleUrl}/>
+        <Embed active url={googleUrl} />
       </div>;
   }
 }

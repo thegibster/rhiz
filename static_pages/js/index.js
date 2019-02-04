4b77680b47741903ca99a2b1e2f98926ca3124a1
andrew gibbs <andrewgibbs@outlook.com>
2019-02-04 09:11:35 -0800

    added static pages

---
+++ static_pages/js/index.js
@@ -0,0 +1,8 @@
+ // $('#datepicker').datepicker({
+ //        weekStart: 1,
+ //        daysOfWeekHighlighted: "6,0",
+ //        autoclose: true,
+ //        todayHighlight: true,
+ //    });
+ //    $('#datepicker').datepicker("setDate", new Date());
+$('#datepicker').datepicker('show');
